92d176bd562639ee5a446fea97df0bfa7582e3a1
sd19022007 <sd19022007@gmail.com>
2025-12-06 11:02:56 -0500

    new hik

--- hik_driver.py
+++ hik_driver.py
@@ -1,15 +1,38 @@
-# -- coding: utf-8 --
-
 '''
-CONFIGURED FOR LINUX NEEDS TO BE CONFIGURED FOR WINDOWS
-NEEDS WORK
+INCOMPLETE WINDOWS CODE, NEEDS WORK
+CANT FIND DLL FILES
 '''
 
+
+
+import os
+import sys
 import cv2
 import numpy as np
 
+MVS_ROOT = r"C:\Users\sd190\temp\MVS"
 
-from MVS.Samples.aarch64.Python.MvImport.MvCameraControl_class import *
+def find_file(root: str, filename: str) -> str | None:
+    for dirpath, _, filenames in os.walk(root):
+        if filename in filenames:
+            return os.path.join(dirpath, filename)
+    return None
+
+wrapper_py = find_file(MVS_ROOT, "MvCameraControl_class.py")
+if not wrapper_py:
+    raise FileNotFoundError(
+        "Could not find MvCameraControl_class.py under:\n"
+        f"  {MVS_ROOT}\n"
+        "Your extracted/installed MVS tree doesn't contain the Python wrapper where expected."
+    )
+print(wrapper_py)
+
+wrapper_dir = os.path.dirname(wrapper_py)
+sys.path.insert(0, wrapper_dir)
+print("Using Python wrapper from:", wrapper_dir)
+
+from MvCameraControl_class import *
+
 g_bExit = False
 
 
@@ -20,7 +43,6 @@ def hik_init():
     deviceList = MV_CC_DEVICE_INFO_LIST()
     tlayerType = MV_GIGE_DEVICE | MV_USB_DEVICE
 
-    # ch:枚举设备 | en:Enum device
     ret = MvCamera.MV_CC_EnumDevices(tlayerType, deviceList)
     if ret != 0:
         print("enum devices fail! ret[0x%x]" % ret)
@@ -33,8 +55,7 @@ def hik_init():
     print("Find %d devices!" % deviceList.nDeviceNum)
 
     for i in range(0, deviceList.nDeviceNum):
-        mvcc_dev_info = cast(deviceList.pDeviceInfo[i], POINTER(
-            MV_CC_DEVICE_INFO)).contents
+        mvcc_dev_info = cast(deviceList.pDeviceInfo[i], POINTER(MV_CC_DEVICE_INFO)).contents
         if mvcc_dev_info.nTLayerType == MV_GIGE_DEVICE:
             print("\ngige device: [%d]" % i)
             strModeName = ""
@@ -42,14 +63,12 @@ def hik_init():
                 strModeName = strModeName + chr(per)
             print("device model name: %s" % strModeName)
 
-            nip1 = (
-                (mvcc_dev_info.SpecialInfo.stGigEInfo.nCurrentIp & 0xff000000) >> 24)
-            nip2 = (
-                (mvcc_dev_info.SpecialInfo.stGigEInfo.nCurrentIp & 0x00ff0000) >> 16)
-            nip3 = (
-                (mvcc_dev_info.SpecialInfo.stGigEInfo.nCurrentIp & 0x0000ff00) >> 8)
+            nip1 = ((mvcc_dev_info.SpecialInfo.stGigEInfo.nCurrentIp & 0xff000000) >> 24)
+            nip2 = ((mvcc_dev_info.SpecialInfo.stGigEInfo.nCurrentIp & 0x00ff0000) >> 16)
+            nip3 = ((mvcc_dev_info.SpecialInfo.stGigEInfo.nCurrentIp & 0x0000ff00) >> 8)
             nip4 = (mvcc_dev_info.SpecialInfo.stGigEInfo.nCurrentIp & 0x000000ff)
             print("current ip: %d.%d.%d.%d\n" % (nip1, nip2, nip3, nip4))
+
         elif mvcc_dev_info.nTLayerType == MV_USB_DEVICE:
             print("\nu3v device: [%d]" % i)
             strModeName = ""
@@ -67,33 +86,25 @@ def hik_init():
             print("user serial number: %s" % strSerialNumber)
 
     nConnectionNum = 0
-
-    # ch:创建相机实例 | en:Creat Camera Object
     cam = MvCamera()
 
-    # ch:选择设备并创建句柄| en:Select device and create handle
-    stDeviceList = cast(deviceList.pDeviceInfo[int(
-        nConnectionNum)], POINTER(MV_CC_DEVICE_INFO)).contents
+    stDeviceList = cast(deviceList.pDeviceInfo[int(nConnectionNum)], POINTER(MV_CC_DEVICE_INFO)).contents
 
     ret = cam.MV_CC_CreateHandle(stDeviceList)
-
     if ret != 0:
         print("create handle fail! ret[0x%x]" % ret)
         sys.exit()
 
-    # ch:打开设备 | en:Open device
     ret = cam.MV_CC_OpenDevice(MV_ACCESS_Exclusive, 0)
     if ret != 0:
         print("open device fail! ret[0x%x]" % ret)
         sys.exit()
 
-    # ch:开始取流 | en:Start grab image
     ret = cam.MV_CC_StartGrabbing()
     if ret != 0:
         print("start grabbing fail! ret[0x%x]" % ret)
         sys.exit()
 
-    # ch:获取数据包大小 | en:Get payload size
     stParam = MVCC_INTVALUE()
     memset(byref(stParam), 0, sizeof(MVCC_INTVALUE))
 
@@ -103,21 +114,19 @@ def hik_init():
         sys.exit()
 
     nPayloadSize = stParam.nCurValue
-    data_buf = (c_ubyte * nPayloadSize)()  # image buffer
+    data_buf = (c_ubyte * nPayloadSize)()
     stFrameInfo = MV_FRAME_OUT_INFO_EX()
     memset(byref(stFrameInfo), 0, sizeof(stFrameInfo))
 
     info_lst = [cam, data_buf, nPayloadSize, stFrameInfo]
-    # check frame readability
-    for i in range(5):
-        ret = cam.MV_CC_GetOneFrameTimeout(
-            data_buf, nPayloadSize, stFrameInfo, 5000) # modified the nMsec=1000 to 5000, 1000 failed frequently
+
+    for _ in range(5):
+        ret = cam.MV_CC_GetOneFrameTimeout(data_buf, nPayloadSize, stFrameInfo, 5000)
         if ret != 0:
-            print("pipline broke while testing frame readability", ret)
+            print("pipeline broke while testing frame readability", ret)
             sys.exit()
 
     return info_lst
-    # -------------------------------------------
 
 
 def read_hik_frame(info_lst):
@@ -126,51 +135,36 @@ def read_hik_frame(info_lst):
     nPayloadSize = info_lst[2]
     stFrameInfo = info_lst[3]
 
-    ret = cam.MV_CC_GetOneFrameTimeout(
-        data_buf, nPayloadSize, stFrameInfo, 1000)
+    ret = cam.MV_CC_GetOneFrameTimeout(data_buf, nPayloadSize, stFrameInfo, 1000)
     if ret == 0:
-        # print("get one frame: Width[%d], Height[%d], nFrameNum[%d], enPixelType[%d]" % (
-        #     stFrameInfo.nWidth, stFrameInfo.nHeight, stFrameInfo.nFrameNum, stFrameInfo.enPixelType))
-
-        image = np.asarray(data_buf).reshape(
-            (stFrameInfo.nHeight, stFrameInfo.nWidth, -1))
+        image = np.asarray(data_buf).reshape((stFrameInfo.nHeight, stFrameInfo.nWidth, -1))
         rgb_image = cv2.cvtColor(image, cv2.COLOR_BAYER_RG2RGB)
-
-        # cv2.imshow("show", rgb_image)
-        # k = cv2.waitKey(1) & 0xff
         return rgb_image
     else:
         print("no data[0x%x] --- Hik" % ret)
+        return None
 
 
 def hik_close(info_lst):
     cam = info_lst[0]
     data_buf = info_lst[1]
-    # ch:停止取流 | en:Stop grab image
+
     ret = cam.MV_CC_StopGrabbing()
     if ret != 0:
         print("stop grabbing fail! ret[0x%x]" % ret)
         del data_buf
         sys.exit()
 
-    # ch:关闭设备 | Close device
     ret = cam.MV_CC_CloseDevice()
     if ret != 0:
-        print("close deivce fail! ret[0x%x]" % ret)
+        print("close device fail! ret[0x%x]" % ret)
         del data_buf
         sys.exit()
 
-    # ch:销毁句柄 | Destroy handle
     ret = cam.MV_CC_DestroyHandle()
     if ret != 0:
         print("destroy handle fail! ret[0x%x]" % ret)
         del data_buf
         sys.exit()
+
     del data_buf
-
-
-# info_lst = hik_init()
-# while True:
-#     read_hik_frame(info_lst)
-#
-# hik_close(info_lst)
